8a81798a8fdfa722fbe3b2c96be448d89cbebe89
Muhammad Harith <harith781@gmail.com>
2020-09-03 14:49:19 +0800

    adding student repo

---
+++ Student Management/Repository/IRepository/IStudentRepository.cs
@@ -0,0 +1,27 @@
+﻿using Student_Management.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Student_Management.Repository.IRepository
+{
+    public interface IStudent
+    {
+        ICollection<Student> GetStudents();
+
+        Student GetStudent(int studentId);
+
+        bool isStudentExists(string name);
+
+        bool isStudentExists(int id);
+
+        bool CreateStudent(Student student);
+
+        bool UpdateStudent(Student student);
+
+        bool DeleteStudent(Student student);
+
+        bool Save();
+    }
+}
---
+++ Student Management/Repository/StudentRepository.cs
@@ -0,0 +1,67 @@
+﻿using Student_Management.Data;
+using Student_Management.Models;
+using Student_Management.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Student_Management.Repository
+{
+    public class StudentRepository : IStudent
+    {
+        private readonly ApplicationDbContext _db;
+
+        public StudentRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public bool CreateStudent(Student student)
+        {
+            _db.Students.Add(student);
+            return Save();
+        }
+
+        public bool DeleteStudent(Student student)
+        {
+            _db.Students.Remove(student);
+            return Save();
+        }
+
+        public Student GetStudent(int studentId)
+        {
+            var student = _db.Students.FirstOrDefault(x => x.Id == studentId);
+            return student;
+        }
+
+        public ICollection<Student> GetStudents()
+        {
+            var students = _db.Students.OrderBy(x => x.Name).ToList();
+            return students;
+        }
+
+        public bool isStudentExists(string name)
+        {
+            bool value = _db.Students.Any(x => x.Name.ToLower().Trim() == name.ToLower().Trim());
+            return value;
+        }
+
+        public bool isStudentExists(int id)
+        {
+            bool value = _db.Students.Any(x => x.Id == id);
+            return value;
+        }
+
+        public bool Save()
+        {
+            return _db.SaveChanges() >= 0 ? true : false;
+        }
+
+        public bool UpdateStudent(Student student)
+        {
+            _db.Students.Update(student);
+            return Save();
+        }
+    }
+}
