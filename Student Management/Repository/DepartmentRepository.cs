06fb24b7df16911f242dc2c8bf9a6a722230ee2d
Muhammad Harith <harith781@gmail.com>
2020-09-03 14:27:02 +0800

    adding department repo

---
+++ Student Management/Repository/DepartmentRepository.cs
@@ -0,0 +1,67 @@
+﻿using Student_Management.Data;
+using Student_Management.Models;
+using Student_Management.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Student_Management.Repository
+{
+    public class DepartmentRepository : IDepartmentRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public DepartmentRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public bool CreateDepartment(Department department)
+        {
+            _db.Departments.Add(department);
+            return Save();
+        }
+
+        public bool DeleteDepartment(Department department)
+        {
+            _db.Remove(department);
+            return Save();
+        }
+
+        public Department GetDepartment(int departmentId)
+        {
+            var department = _db.Departments.FirstOrDefault(x => x.Id == departmentId);
+            return department;
+        }
+
+        public ICollection<Department> GetDepartments()
+        {
+            return _db.Departments.OrderBy(x => x.Name).ToList();
+        }
+
+        public bool isDepartmentExists(string name)
+        {
+            bool value = _db.Departments.Any(x => x.Name.ToLower().Trim() == name.ToLower().Trim());
+            return value;
+        }
+
+        public bool isDepartmentExists(int id)
+        {
+            bool value = _db.Departments.Any(x => x.Id == id);
+            return value;
+        }
+
+        public bool Save()
+        {
+            return _db.SaveChanges() >= 0 ? true : false;
+        }
+
+        public bool UpdateDepartment(Department department)
+        {
+            _db.Departments.Update(department);
+            return Save();
+        }
+    }
+}
---
+++ Student Management/Repository/IRepository/IDepartmentRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualBasic;
+using Student_Management.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Student_Management.Repository.IRepository
+{
+    public interface IDepartmentRepository
+    {
+        ICollection<Department> GetDepartments();
+
+        Department GetDepartment(int departmentId);
+
+        bool isDepartmentExists(string name);
+
+        bool isDepartmentExists(int id);
+
+        bool CreateDepartment(Department department);
+
+        bool UpdateDepartment(Department department);
+
+        bool DeleteDepartment(Department department);
+
+        bool Save();
+    }
+}
