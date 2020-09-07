b20a2d380aaff7e51d2fa9fbd98eeb99cfa5f91f
Muhammad Harith <harith781@gmail.com>
2020-09-07 16:24:32 +0800

    adding automapper class

---
+++ Student Management/AutoMapper/ManagementMapper.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Student_Management.Models;
+using Student_Management.Models.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Student_Management.AutoMapper
+{
+    public class ManagementMapper : Profile
+    {
+        public ManagementMapper()
+        {
+            CreateMap<Department, DepartmentDto>().ReverseMap();
+            CreateMap<Teacher, TeacherDto>().ReverseMap();
+            CreateMap<Student, StudentDto>().ReverseMap();
+        }
+    }
+}
--- Student Management/Startup.cs
+++ Student Management/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Student_Management.AutoMapper;
 using Student_Management.Data;
 
 namespace Student_Management
@@ -29,6 +31,9 @@ namespace Student_Management
             services.AddControllers();
             services.AddDbContext<ApplicationDbContext>
                 (options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+
+            services.AddAutoMapper(typeof(ManagementMapper));
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
