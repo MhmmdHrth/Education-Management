04232d17c294e70849cc27492e7575164e40b6fd
Muhammad Harith <harith781@gmail.com>
2020-09-07 16:19:47 +0800

    adding dto model

---
+++ Student Management/Models/Dtos/StudentDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Student_Management.Models.Dtos
+{
+    public class StudentDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public int Age { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Phone]
+        public int PhoneNumber { get; set; }
+
+        [Required]
+        public int TeacherId { get; set; }
+    }
+}
