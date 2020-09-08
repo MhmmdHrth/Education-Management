de115c829df7af9ef7dd9e53ddf13406d843cf39
Muhammad Harith <harith781@gmail.com>
2020-09-08 15:14:39 +0800

    adding crud teacher controller

---
+++ Student Management/Controllers/TeacherController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Student_Management.Models;
+using Student_Management.Models.Dtos;
+using Student_Management.Repository.IRepository;
+
+namespace Student_Management.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeacherController : ControllerBase
+    {
+        private readonly ITeacherRepository _teacherRepo;
+        private readonly IMapper _mapper;
+
+        public TeacherController(ITeacherRepository teacherRepo, IMapper mapper)
+        {
+            _teacherRepo = teacherRepo;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetTeachers()
+        {
+            var objList = _teacherRepo.GetTeachers();
+            var objDto = new List<TeacherDto>();
+
+            foreach(var obj in objList)
+            {
+                objDto.Add(_mapper.Map<TeacherDto>(obj));
+            }
+
+            return Ok(objDto);
+        }
+
+        [HttpGet("{teacherId:int}",Name = nameof(GetTeacher))] //{uniqueName:type}
+        public IActionResult GetTeacher(int teacherId)
+        {
+            var obj = _teacherRepo.GetTeacher(teacherId);
+
+            if(obj == null)
+            {
+                return NotFound();
+            }
+
+            var objDto = _mapper.Map<TeacherDto>(obj);
+            return Ok(objDto);
+        }
+
+        [HttpPost]
+        public IActionResult CreateTeacher(TeacherDto teacherDto)
+        {
+            if(teacherDto == null)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (_teacherRepo.isTeacherExists(teacherDto.Name))
+            {
+                ModelState.AddModelError("", $"Teacher is exists!");
+                return StatusCode(404, ModelState);
+            }
+
+            var teacherObj = _mapper.Map<Teacher>(teacherDto);
+
+            if (!_teacherRepo.CreateTeacher(teacherObj))
+            {
+                ModelState.AddModelError("", $"Something went wrong when saving the record {teacherObj.Name}");
+                return StatusCode(500, ModelState);
+            }
+
+            return CreatedAtRoute(nameof(GetTeacher), new { teacherId = teacherObj.Id }, teacherObj);
+        }
+
+        [HttpPatch]
+        public IActionResult UpdateTeacher(int teacherId, TeacherDto teacherDto)
+        {
+            if(teacherDto == null || teacherId != teacherDto.Id)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var teacherObj = _mapper.Map<Teacher>(teacherDto);
+
+            if (!_teacherRepo.UpdateTeacher(teacherObj))
+            {
+                ModelState.AddModelError("", $"Something went wrong when updating the record {teacherObj.Name}");
+                return StatusCode(500, ModelState);
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteTeacher(int teacherId)
+        {
+            if (!_teacherRepo.isTeacherExists(teacherId))
+            {
+                return NotFound();
+            }
+
+            var teacherObj = _teacherRepo.GetTeacher(teacherId);
+
+            if (!_teacherRepo.DeleteTeacher(teacherObj))
+            {
+                ModelState.AddModelError("", $"Something went wrong when deleting the record {teacherObj.Name}");
+                return StatusCode(500, ModelState);
+            }
+
+            return NoContent();
+        }
+    }
+}
--- Student Management/Repository/IRepository/ITeacherRepository.cs
+++ Student Management/Repository/IRepository/ITeacherRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Student_Management.Repository.IRepository
 {
-    interface ITeacherRepository
+    public interface ITeacherRepository
     {
         ICollection<Teacher> GetTeachers();
 
