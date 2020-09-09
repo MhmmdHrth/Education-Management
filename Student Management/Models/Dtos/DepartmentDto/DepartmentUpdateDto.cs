b4bd64794138f6d795524dc34f00eb8ec2e59855
Muhammad Harith <harith781@gmail.com>
2020-09-09 17:17:13 +0800

    add department update dto model

---
+++ Student Management/Models/Dtos/DepartmentDto/DepartmentUpdateDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Student_Management.Models.Dtos.DepartmentDto
+{
+    public class DepartmentUpdateDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
