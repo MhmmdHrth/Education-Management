eb5041b027d966611731adc0fe3b2e51dc346846
Muhammad Harith <harith781@gmail.com>
2020-11-07 11:05:00 +0800

    adding user repository

---
+++ Student Management/Repository/IRepository/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Student_Management.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Student_Management.Repository.IRepository
+{
+    interface IUserRepository
+    {
+        bool IsUniqueUser(string username);
+
+        User Authenticate(string username, string password);
+
+        User Register(string username, string password);
+    }
+}
---
+++ Student Management/Repository/UserRepository.cs
@@ -0,0 +1,35 @@
+﻿using Student_Management.Data;
+using Student_Management.Models;
+using Student_Management.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Student_Management.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext context;
+
+        public UserRepository(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public User Authenticate(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsUniqueUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Register(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
